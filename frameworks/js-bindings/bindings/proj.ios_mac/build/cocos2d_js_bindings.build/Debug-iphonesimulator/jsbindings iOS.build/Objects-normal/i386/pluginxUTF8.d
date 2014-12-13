891cfe00baa5385b8273f2d5ab9568852c30bf27
Ten Mutunhire <tmutunhire@gmail.com>
2014-12-13 23:25:14 +0000

    initial game commit

---
+++ frameworks/js-bindings/bindings/proj.ios_mac/build/cocos2d_js_bindings.build/Debug-iphonesimulator/jsbindings iOS.build/Objects-normal/i386/pluginxUTF8.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/tmutunhire/Desktop/tools/cocos2d-js-v3.1/frameworks/js-bindings/cocos2d-x/plugin/jsbindings/manual/pluginxUTF8.cpp \
+  /Users/tmutunhire/Desktop/tools/cocos2d-js-v3.1/frameworks/js-bindings/cocos2d-x/plugin/jsbindings/manual/pluginxUTF8.h
